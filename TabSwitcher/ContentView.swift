fe9c9d3361d5e4416d75fceecc90f4d47b5c748c
Danika Gupta <danika@gprof.com>
2023-07-22 09:23:35 -0700

    Initial commit

--- TabSwitcher/ContentView.swift
+++ TabSwitcher/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelected=1
+    var body: some View {
+        TabView(selection:$tabSelected) {
+            Tab1View(tabSelect:$tabSelected)
+                .tabItem{
+                    Label("Tab 1",systemImage: "list.number")
+                }
+                .tag(1)
+            Tab2View(tabSelect:$tabSelected)
+                .tabItem{
+                    Label("Tab 2",systemImage: "list.number")
+                }
+                .tag(2)
+            Tab3View(tabSelect:$tabSelected)
+                .tabItem{
+                    Label("Tab 3",systemImage: "list.number")
+                }
+                .tag(3)
+        }
+    }
+}
+
+struct Tab1View: View {
+    @Binding var tabSelect:Int
+    var body: some View {
+        
+        VStack {
+            Text("Page 1")
+                
+            Button("Change to Tab 2") {
+                tabSelect=2;
+            }
+            Button("Change to Tab 3") {
+                tabSelect=3;
+            }
+        }
+        .font(.largeTitle)
+    }
+}
+
+struct Tab2View: View {
+    @Binding var tabSelect:Int
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Page 2")
+            Button("Change to Tab 1") {
+                tabSelect=1;
+            }
+            Button("Change to Tab 3") {
+                tabSelect=3;
+            }
         }
-        .padding()
+        .font(.largeTitle)
+    }
+}
+
+struct Tab3View: View {
+    @Binding var tabSelect:Int
+    var body: some View {
+        VStack {
+            Text("Page 3")
+            Button("Change to Tab 1") {
+                tabSelect=1;
+            }
+            Button("Change to Tab 2") {
+                tabSelect=2;
+            }
+        }
+        .font(.largeTitle)
     }
 }
 
