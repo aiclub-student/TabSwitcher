45b7e92e15fa7e37ded4727cca2146a127a6c7ec
Danika Gupta <danika@gprof.com>
2023-07-22 07:19:08 -0700

    Initial Commit

---
+++ TabSwitcher/TabSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabSwitcherApp.swift
+//  TabSwitcher
+//
+//  Created by Amit Gupta on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TabSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
